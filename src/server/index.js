47b164d0d6698e1349d0a1fe5e95932adf132f4c
Dale <daz20589@hotmail.com>
2018-03-09 16:06:16 +1100

    bit of a routing cleanup

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const mongoose = require('mongoose')
+const { initWebpack } = require('./dev')
+const routes = require('./routes')
+
 const app = express()
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -10,40 +13,18 @@ mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`)
 })
 
-if (!isProd) {
-  const webpack = require('webpack')
-  const config = require('../../config/webpack')()
-  const compiler = webpack(config)
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  app.use(webpackDevMiddleware(compiler))
-  app.use(webpackHotMiddleware(compiler, {
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000,
-    timeout: 20000,
-    reload: true,
-    publicPath: config.output.publicPath
-  }))
-
-  app.use('*', function (req, res, next) {
-    const filename = path.join(compiler.outputPath, 'index.html')
-    compiler.outputFileSystem.readFile(filename, function (err, result) {
-      if (err) {
-        return next(err)
-      }
-      res.set('content-type', 'text/html')
-      res.send(result)
-      res.end()
-    })
-  })
-} else {
+if (isProd) {
   const publicPath = path.resolve('public')
   app.use('/public', express.static(publicPath))
   app.use('*', function (req, res) {
     const indexPage = path.join(publicPath, 'index.html')
     res.sendFile(indexPage)
   })
+} else {
+  initWebpack(app)
 }
+
+app.use('/', routes)
 
 app.listen(process.env.PORT || 6060, function () {
   console.log(`Express running on port ${this.address().port}`)
